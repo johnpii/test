c40bba78a65e3aaa0af819214bc4982b59f4d7f3
johnpii <103769277+johnpii@users.noreply.github.com>
2022-04-24 19:53:52 +0400

    Delete Abricos.java

--- src/Abricos.java
+++
@@ -1,8 +0,0 @@
-public class Abricos extends Frukt {
-    static double total = 0;
-    @Override
-    public double cost(double weight) {
-        total += weight * 200;
-        return weight * 200;
-    }
-}
