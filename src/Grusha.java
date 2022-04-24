78dc2724d77396e66ec84a2c382ff1b91124bc39
johnpii <103769277+johnpii@users.noreply.github.com>
2022-04-24 19:54:33 +0400

    Delete Grusha.java

--- src/Grusha.java
+++
@@ -1,8 +0,0 @@
-public class Grusha extends Frukt {
-    static double total = 0;
-    @Override
-    public double cost(double weight) {
-        total += weight * 150;
-        return weight * 150;
-    }
-}
