7916cf62545776793ffb2ccdec7d3460a9ecde38
johnpii <103769277+johnpii@users.noreply.github.com>
2022-04-24 20:11:00 +0400

    Delete Frukt.java

--- src/Frukt.java
+++
@@ -1,8 +0,0 @@
-public abstract class Frukt {
-    double weight;
-    public final void printManufacturerInfo()
-    {
-        System.out.print("Made in Ukraine");
-    }
-    public abstract double cost(double weight);
-}
