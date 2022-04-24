048be74f8101e1e36533bdd3c1082f8f0c08861a
johnpii <103769277+johnpii@users.noreply.github.com>
2022-04-24 19:54:02 +0400

    Delete Apple.java

--- src/Apple.java
+++
@@ -1,8 +0,0 @@
-public class Apple extends Frukt {
-    static double total = 0;
-    @Override
-    public double cost(double weight) {
-        total += weight * 100;
-        return weight * 100;
-    }
-}
